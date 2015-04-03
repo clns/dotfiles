65b77ad66a6e6a03e3e56fa3648f6cc40fd2e1e9
Calin Seciu <calinseciu@gmail.com>
2015-04-03 14:40:51 +0300

    Add vim-flow plugin

--- install/node.sh
+++ install/node.sh
@@ -1,6 +1,21 @@
 log_warning "TODO: install node.js"
 
-# Upgrade npm
-if has_flag '-u'; then
-    npm install npm -g
+# Install/update npm
+if ! npm_exists "npm" "-g" || has_flag '-u'; then
+    prompt "Install/update npm"
+    if is_confirmed; then
+        npm install npm -g
+    fi
+elif npm_exists "npm" "-g"; then
+    log_arrow "$(npm ls npm -g | grep npm) is installed"
+fi
+
+# Install/update flow-bin
+if ! npm_exists "flow-bin" "-g" || has_flag '-u'; then
+    prompt "Install/update flow-bin."
+    if is_confirmed; then
+        npm install flow-bin -g
+    fi
+elif npm_exists "flow-bin" "-g"; then
+    log_arrow "$(npm ls flow-bin -g | grep flow-bin) is installed"
 fi
---
+++ link/.vim/plugin/settings/vim-flow.vim
@@ -0,0 +1,3 @@
+" https://github.com/facebook/vim-flow
+
+let g:flow#autoclose = 1 " close the |quickfix| window automatically
