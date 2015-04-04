305625d350d504882f9dace57b3f4a021a28a66c
Calin Seciu <calinseciu@gmail.com>
2015-04-04 11:07:44 +0300

    Remove temporary exit

--- source/go.sh
+++ source/go.sh
@@ -3,5 +3,4 @@ if is_osx && formula_exists 'go'; then
     export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
 elif is_ubuntu; then
     log_error "TODO: implement this for Ubuntu"
-    exit 1
 fi
