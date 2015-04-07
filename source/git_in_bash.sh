8859882a0fa80b343e9baa47c1c632cf7b10a45f
Calin Seciu <calinseciu@gmail.com>
2015-04-07 20:47:43 +0300

    Add git completion and prompt for bash (Git in Bash)

---
+++ source/git_in_bash.sh
@@ -0,0 +1,23 @@
+# http://git-scm.com/book/en/v2/Git-in-Other-Environments-Git-in-Bash
+
+alias g='git'
+
+GIT_COMPLETION="$HOME/.git-completion.bash"
+GIT_PROMPT="$HOME/.git-prompt.sh"
+
+# https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+if [ -e "$GIT_COMPLETION" ]; then
+    source "$GIT_COMPLETION"
+    complete -o default -o nospace -F _git g
+fi
+
+# https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+if [ -e "$GIT_PROMPT" ]; then
+    source "$GIT_PROMPT"
+    export GIT_PS1_SHOWDIRTYSTATE=1
+    export GIT_PS1_SHOWSTASHSTATE=1
+    export GIT_PS1_SHOWUPSTREAM="auto"
+    export GIT_PS1_SHOWCOLORHINTS=1
+    # export PS1='\w$(__git_ps1 " (%s)")\$ '
+    PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
+fi
