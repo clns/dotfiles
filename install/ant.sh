97b7fb700df28797334f6437e576f962efc2476d
Calin Seciu <calinseciu@gmail.com>
2015-05-13 15:44:09 +0300

    Add 'ant' install script

---
+++ install/ant.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Apache Ant
+#
+# http://ant.apache.org/
+
+script_name=ant.sh
+script_tmp=ant-install-$$.sh
+
+if [ "x$0" = "xsh" ]; then
+  # run as curl | sh
+  # on some systems, you can just do cat>npm-install.sh
+  # which is a bit cuter.  But on others, &1 is already closed,
+  # so catting to another script file won't do anything.
+  # Follow Location: headers, and fail on errors
+  curl -f -L -s \
+    https://raw.githubusercontent.com/clns/dotfiles/master/install/$script_name \
+    > $script_tmp
+  ret=$?
+  if [ $ret -eq 0 ]; then
+    (exit 0)
+  else
+    rm $script_tmp
+    echo "Failed to download script" >&2
+    exit $ret
+  fi
+  sh $script_tmp
+  ret=$?
+  rm $script_tmp
+  exit $ret
+fi
+
+case "$(uname -s)" in
+
+  Darwin)
+    brew install ant
+    ;;
+
+  Linux)
+    if [ $(which apt-get) ]; then
+      sudo apt-get -u install ant
+    elif [ $(which yum) ]; then
+      echo "nothing to do for yum?!"
+    else
+      echo "apt-get or yum not found, cannot install $script_name" >&2
+      exit 1
+    fi
+    ;;
+
+  *)
+    echo "$script_name is only supported on Mac OS X and Linux" >&2
+    exit 1
+    ;;
+esac
