b1600ad47fb2dce00b65ec062c7db467cffe15f1
Calin Seciu <calinseciu@gmail.com>
2015-05-26 01:43:19 +0300

    Add custom aliases file

---
+++ source/aliases.sh
@@ -0,0 +1,11 @@
+# `z <dir>` - cd to a directory from ~/Projects/
+_z() {
+    cd "$HOME/Projects/$1"
+}
+alias z=_z
+_z_completion() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=($(compgen -W "$(ls -ap $HOME/Projects/ | grep -E '^[^.]{2}.*/')" -- ${cur}))
+    return 0
+}
+complete -F _z_completion z
