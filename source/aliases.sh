77e0943a46c64c38e13fc0c3bd8ef3d8fabe998f
Calin Seciu <calinseciu@gmail.com>
2015-10-21 20:53:57 +0300

    Add docker cleanup commands
    Credits: http://blog.stefanxo.com/2014/02/clean-up-after-docker/

--- source/aliases.sh
+++ source/aliases.sh
@@ -3,3 +3,9 @@ _z() {
     cd "$HOME/Projects/$1"
 }
 alias z=_z
+
+# docker cleanup commands
+# http://blog.stefanxo.com/2014/02/clean-up-after-docker/
+alias dockercleancontainers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs docker rm"
+alias dockercleanimages="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs docker rmi"
+alias dockerclean="dockercleancontainers && dockercleanimages"
